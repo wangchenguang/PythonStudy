8dff25b12d0b7cd37ea8f772b9ee9c8303310cca
wangchg <wangchg@hotmail.com>
2019-01-15 16:54:34 +0800

    first commit

--- pandas_study/clearData.py
+++ pandas_study/clearData.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pandas import DataFrame
+
 data = {'Chinese': [66, 95, 93, 90, 80], 'English': [65, 85, 92, 88, 90], 'Math': [30, 98, 96, 77, 90]}
 # 创建表数据机构
 df2 = DataFrame(data, index=['ZhangFei', 'GuanYu', 'ZhaoYuan', 'HuangZhong', 'DianWei'],
